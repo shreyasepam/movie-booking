f9602b6b23af7b38947b5d049ccab8dcaeb10bd6
shreyasepam <shreyas_bm@epam.com>
2024-11-03 13:18:04 +0530

    handling not found status

--- src/screens/movieDetails/index.tsx
+++ src/screens/movieDetails/index.tsx
@@ -1,25 +1,38 @@
 import React, { useEffect } from "react";
 import IMovieDetailsScreenProps from "./IMovieDetails.props";
 import MovieBanner from "../../components/movieBanner";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useAppDispatch } from "../../redux/reduxHooks";
 import { getMovieById } from "../../redux/slice/movieByIdSlice";
 import MovieOverview from "../../components/movieOverview";
+import { toast } from "react-toastify";
 
 const MovieDetails: React.FC<IMovieDetailsScreenProps> = () => {
+  const navigation = useNavigate();
   const dispatch = useAppDispatch();
   const { id } = useParams();
 
-  useEffect(() => {
-    if (id) {
-      dispatch(getMovieById({ id }));
+  const fetchById = async (): Promise<void> => {
+    try {
+      if (id) {
+        const response = await dispatch(getMovieById({ id }));
+        if (response.meta.requestStatus === "rejected") {
+          navigation("/not-found", { replace: true });
+        }
+      }
+    } catch (ex) {
+      toast("Somthing went wrong.");
     }
+  };
+
+  useEffect(() => {
+    fetchById();
   }, [id]);
 
   return (
-    <div >
+    <div>
       <MovieBanner />
-      <MovieOverview /> 
+      <MovieOverview />
     </div>
   );
 };
--- src/screens/notFound/index.tsx
+++ src/screens/notFound/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import INotFoundProps from './INotFound.props';
+import React from "react";
+import INotFoundProps from "./INotFound.props";
+import { Link } from "react-router-dom";
 
-const NotFound:React.FC<INotFoundProps> = () => {
+const NotFound: React.FC<INotFoundProps> = () => {
   return (
-    <div>NotFound</div>
-  )
-}
+    <div className="text-center bg-blue-base p-6 rounded">
+      <p className="text-white font-semibold text-lg">
+        Content not found.{" "}
+        <Link className="underline " to={"/"}>
+          {"Go home🎥"}
+        </Link>
+      </p>
+    </div>
+  );
+};
 
 export default NotFound;
