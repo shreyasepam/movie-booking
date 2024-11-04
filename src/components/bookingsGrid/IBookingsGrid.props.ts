11cff900e63adefec7d24391ef126a9787e9b83b
shreyasepam <shreyas_bm@epam.com>
2024-11-04 11:33:28 +0530

    revert the props

--- src/components/bookingsGrid/IBookingsGrid.props.ts
+++ src/components/bookingsGrid/IBookingsGrid.props.ts
@@ -1,44 +1,4 @@
-export interface IBookingTime {
-    id:string,
-    cost: number,
-    time: string
-}
-
-export interface IMovie {
-    adult: boolean;
-    backdrop_path: string | null;
-    genre_ids: number[];
-    id: number;
-    original_language: string;
-    original_title: string;
-    overview: string;
-    popularity: number;
-    poster_path: string | null;
-    release_date: string;
-    title: string;
-    video: boolean;
-    vote_average: number;
-    vote_count: number;
-}
-
-export interface IMovieAPIResponse {
-    page?: number;
-    results?: IMovie[];
-    total_pages?: number;
-    total_results?: number;
-}
-
-export interface IBookingsData {
-  id?: string;
-  movie?: IMovie;
-  slot?: IBookingTime;
-  seats: number[];
-  dateTime: string;
-  user?: string;
-}
-
-export type BookingMode = "new" | "delete" | "read";
-
+import { IBookingsData } from "../../redux/interface/Bookings";
 
 export default interface IBookingsGridProps{
     booking: IBookingsData;
